40b90ecea9444b1285d9e14bd037c40248b0d3aa
RedLuke17 <lukeishere17@gmail.com>
2022-11-20 09:29:28 +0100

    test sito

---
+++ MainSite/static/js/sett.js
@@ -0,0 +1,5 @@
+function button() {
+    let hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('open');
+    document.querySelector("body").classList.toggle("open");
+}
