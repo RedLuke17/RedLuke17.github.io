ece0af00d73a2452a0558df82461c65ed5b58154
RedLuke17 <lukeishere17@gmail.com>
2022-11-22 18:46:00 +0100

    settaggi home finiti

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,19 @@
+const body = document.querySelector("body");
 const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu-list");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 
 menuBtn.onclick = ()=>{
-    menu.classList.add("active")
-    menuBtn.classList.add("hide")
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabledScroll");
 }
 
 cancelBtn.onclick = ()=>{
-    menu.classList.remove("active")
-    menuBtn.classList.remove("hide")
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    body.classList.remove("disabledScroll");
 }
 
 window.onscroll = ()=>{
